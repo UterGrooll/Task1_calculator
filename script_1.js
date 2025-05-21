151b16cf64dc0725f74816cb29dceac3a673faa1
UterGrooll <utergrooll@yandex.ru>
2025-05-22 00:10:04 +0300

    Поигрался с кнопками

--- script_1.js
+++ script_1.js
@@ -1,13 +1,1 @@
 
-console.log("Привет из внешнего файла!");
-
-    let userName = "UterGrooll";
-    const age = 35;
-    var isAdmin = true;
-
-console.log("Имя пользователя:", userName);
-console.log("Возраст:", age);
-console.log("Админ?", isAdmin);
-console.log("typeof userName:", typeof userName); // string
-console.log("typeof age:", typeof age);           // number
-console.log("typeof isAdmin:", typeof isAdmin);   // boolean
